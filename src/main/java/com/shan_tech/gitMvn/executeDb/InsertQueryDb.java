552c94542574a5737467d50ac5a84549c4169d01
shanmukh-v-i <shanmukh.v.i@gmail.com>
2020-12-19 19:55:15 +0530

    insert Statement

---
+++ src/main/java/com/shan_tech/gitMvn/executeDb/InsertQueryDb.java
@@ -0,0 +1,25 @@
+package com.shan_tech.gitMvn.executeDb;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import com.shan_tech.gitMvn.connectionClass.ConnectDb;
+
+public class InsertQueryDb {
+	public static void main(String args[]) {
+		
+		Connection connection = null;
+        Statement insertStmt = null;
+        try {
+        	connection=ConnectDb.getCon();
+        	insertStmt=connection.createStatement();
+        	insertStmt.execute("INSERT INTO EMPLOYEE (EMAIL_ADDRESS,FIRST_NAME,LAST_NAME) VALUES ('shan@shan-tech.com','shan','inamdar')");
+            insertStmt.execute("INSERT INTO EMPLOYEE (EMAIL_ADDRESS,FIRST_NAME,LAST_NAME) VALUES ('amit@shan-tech','amit','inamdar')");
+        	
+        }
+        catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
